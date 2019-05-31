942bd84c7a03688063224af72496914732e306f2
jonnylagos115 <lagosjonathan1@gmail.com>
2019-05-30 17:29:44 -0700

    Add files via upload

---
+++ libft_1/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlagos <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/29 10:59:27 by jlagos            #+#    #+#             */
+/*   Updated: 2018/12/12 17:01:53 by jlagos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(dst);
+	if (len > dstsize)
+		return (dstsize + ft_strlen(src));
+	while (src[i])
+	{
+		if (dstsize - 1 > len)
+			dst[len] = src[i];
+		else
+		{
+			dst[len] = '\0';
+			break ;
+		}
+		i++;
+		len++;
+	}
+	dst[len] = '\0';
+	return (len);
+}
