0a8c361597607385ea9e8f69ef7b218c6766c5b9
jonnylagos115 <lagosjonathan1@gmail.com>
2019-08-08 19:02:52 -0700

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -16,12 +16,12 @@
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 # define UNSIGNED_INT_MAX (unsigned int)~0
-# define LONG_MAX (long)((unsigned long)~0 >> 1)
-# define LONG_MIN -(long)((unsigned long)~0 >>1) -1
+# define LLONG_MAX (long)((unsigned long)~0 >> 1)
+# define LLONG_MIN -(long)((unsigned long)~0 >>1) -1
 # define ULONG_MAX (unsigned long)~0
 # define CONTAINS_FLAG(x) ('#' == x) || ('0' == x) || ('-' == x) || ('+' == x) || (' ' == x)
-# define IS_BASED_10(x) ('d' == x) || ('i' == x) || ('u' == x)
-# define IS_BASED_16(x) ('x' == x) || ('X' == x) || ('p' == x)
+# define IS_SIGNED(x) ('d' == x) || ('i' == x)
+# define IS_UNSIGNED(x) ('u' == x)|| ('o' == x) || ('x' == x) || ('X' == x)
 # define CONTAINS_LENGTH(x) ('h' == x ) || ('l' == x) || ('L' == x)
 # define SIZE(x) sizeof(x)*8
 # include <stdarg.h>
@@ -36,23 +36,37 @@ typedef struct s_fspecifier
 	char		flag_s;
 	int			width_s;
 	int			precision;
-	char		length_s;
+	int			length_s;
 	int			format_s;
 	int			num_args;
 }				t_format_s;
 
-typedef enum format_spec {
-	C = 1 << 0,
-	S = 1 << 1,
-	P = 1 << 2,
-	D = 1 << 3,
-	I = 1 << 4,
-	O = 1 << 5,
-	U = 1 << 6,
-	x = 1 << 7,
-	X = 1 << 8,
-}f_spec;
+enum 		s_length_modifier {
+	NONE = 1 << 0,
+	HH = 1 << 1,
+	H = 1 << 2,
+	l = 1 << 3,
+	ll = 1 << 4,
+	L = 1 << 5
+};
+
 void		parse_f_specifiers(const char **fmt, t_format_s *fs);
 void		print_args(t_format_s *ret, va_list args, int *n);
-
+intmax_t	get_signed_nbr(va_list args, int length_s);
+uintmax_t	get_unsigned_nbr(va_list args, int length_s);
+void		print_signed_nbr(intmax_t n);
+void		print_unsigned_nbr(uintmax_t n, char fs);
 #endif
+
+/*
+042
+9223372036854775807
+-9223372036854775808
+042
+9223372036854775807
+-9223372036854775808
+0, 18446744073709551615
+0, 1777777777777777777777
+0, ffffffffffffffff
+0, FFFFFFFFFFFFFFFF
+ */
