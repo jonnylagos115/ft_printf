48922fa49fe21bbde8b30d61d87750aedfaceaee
jonnylagos115 <lagosjonathan1@gmail.com>
2020-09-01 17:07:46 -0700

    Add files via upload

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlagos <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/29 12:00:33 by jlagos            #+#    #+#             */
+/*   Updated: 2018/11/18 16:38:08 by jlagos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*last_c;
+	size_t		i;
+
+	i = ft_strlen(s);
+	last_c = NULL;
+	while (s[i] != (unsigned char)c)
+	{
+		if (i == 0)
+			return (NULL);
+		i--;
+	}
+	last_c = &s[i];
+	return ((char *)last_c);
+}
